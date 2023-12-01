f5e5caed64d5248ee5db1eb51e087aa5fd16a28d
Princewenwen <40328534+Princewenwen@users.noreply.github.com>
2023-12-01 01:17:29 -0500

    Updated file that can upload to firebase

--- WIFI.ino
+++ WIFI.ino
@@ -1,85 +1,49 @@
 #include <WiFiNINA.h>
 #include <MKRIMU.h>
+#include <Firebase_Arduino_WiFiNINA.h>
 
 char ssid[] = "s23";        // your network SSID (name)
 char pass[] = "1234567890"; // your network password (use for WPA, or use as key for WEP)
+#define FIREBASE_HOST "https://realtime-imu-default-rtdb.firebaseio.com/" // Replace with your Firebase database domain
+#define FIREBASE_AUTH "PKbdVFzXV3X5SeqVb2QhGVbvkS0ZACLU1WcCUFE2"           // Replace with your Firebase database secret
+
 int status = WL_IDLE_STATUS; // the Wifi radio's status
 
+FirebaseData firebaseData;
+
 void setup() {
-  // Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Connect to Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, ssid, pass);
 
   // Initialize IMU
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
-
-  // Attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
-    delay(10000);
-  }
-
-  Serial.println("You're connected to the network");
-  Serial.println("----------------------------------------");
-  printWifiData();  // Print WiFi data only once after connection
-  Serial.println("----------------------------------------");
 }
 
 void loop() {
-  // Update IMU data more frequently
-  printIMUData();
+  float ax, ay, az;
 
-  // Small delay to make the output readable
-  delay(500); 
-}
-
-void printWifiData() {
-  // Print board and network information
-  Serial.println("Board Information:");
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-  Serial.println();
-  Serial.println("Network Information:");
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
-}
-
-void printIMUData() {
-  // Check if IMU data is available
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
-    float ax, ay, az;
-    float gx, gy, gz;
-
-    // Read acceleration and gyroscope
+  if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(ax, ay, az);
-    IMU.readGyroscope(gx, gy, gz);
 
-    // Print IMU data
-    Serial.print("Acceleration - X: ");
-    Serial.print(ax);
-    Serial.print(", Y: ");
-    Serial.print(ay);
-    Serial.print(", Z: ");
-    Serial.println(az);
-    Serial.print("Gyroscope - X: ");
-    Serial.print(gx);
-    Serial.print(", Y: ");
-    Serial.print(gy);
-    Serial.print(", Z: ");
-    Serial.println(gz);
-    Serial.println();
+    // Send data to Firebase
+    Firebase.setFloat(firebaseData, "/imu/ax", ax);
+    Firebase.setFloat(firebaseData, "/imu/ay", ay);
+    Firebase.setFloat(firebaseData, "/imu/az", az);
   }
+
+  delay(2000); // Delay between updates
 }
